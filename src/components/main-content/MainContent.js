792c5d9a408631243680c089efdc79058569956d
zatkiller <gheedylan@yahoo.com.sg>
2020-12-27 04:04:52 +0800

    Added page number functionalities

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './MainContent.scss';
 import Slideshow from '../slide-show/Slideshow';
@@ -17,13 +17,23 @@ const MainContent = () => {
     }
   ];
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
         <div className="paginate">
-          <Paginate />
+          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
     </div>
--- src/components/paginate/Paginate.js
+++ src/components/paginate/Paginate.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Paginate.scss';
 
-const Paginate = () => {
+const Paginate = (props) => {
+  const { currentPage, totalPages, paginate } = props;
+  const [page, setPage] = useState();
+  const [totalPageNumber, setTotalPageNumber] = useState();
+
+  useEffect(() => {
+    setPage(currentPage);
+    setTotalPageNumber(totalPages);
+  }, [currentPage, totalPages]);
+
   return (
     <>
-      <span className="pageCount">1 - 20</span>
-      <button className="paginate-button disable">Prev</button>
-      <button className="paginate-button">Next</button>
+      <span className="pageCount">
+        {page} - {totalPageNumber}
+      </span>
+      <button className={page > 1 ? 'paginate-button' : 'paginate-button disable'} onClick={() => paginate('prev')}>
+        Prev
+      </button>
+      <button className={page === totalPageNumber ? 'paginate-button disable' : 'paginate-button'} onClick={() => paginate('next')}>
+        Next
+      </button>
     </>
   );
 };

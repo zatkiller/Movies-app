be0fec3f72160e2c8a70a02322f1d6d172ece7ad
zatkiller <gheedylan@yahoo.com.sg>
2020-12-27 04:47:48 +0800

    Finished with Grid

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -13,6 +13,18 @@ const Grid = (props) => {
             <div className="grid-read-more">
               <button className="grid-cell-button">Read More</button>
             </div>
+            <div className="grid-detail">
+              <span className="grid-detail-title">Mission Impossible</span>
+              <div className="grid-detail-rating"></div>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              <i className="fa fa-star" aria-hidden="true"></i>
+              &nbsp;&nbsp;
+              <div className="grid-vote-average">4.5</div>
+            </div>
           </div>
         </React.Fragment>
       ))}

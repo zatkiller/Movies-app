13c36b91caabe9deb661d33bf7db6fcc063aadbb
zatkiller <gheedylan@yahoo.com.sg>
2020-12-26 23:43:13 +0800

    Added Header items using font awesome

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from '../../../src/logo.svg';
 
 import './Header.scss';
 
+const HEADER_LIST = [
+  {
+    id: 1,
+    iconClass: 'fas fa-film',
+    name: 'Now Playing',
+    type: 'now_playing'
+  },
+  {
+    id: 2,
+    iconClass: 'fas fa-fire',
+    name: 'Popular',
+    type: 'popular'
+  },
+  {
+    id: 3,
+    iconClass: 'fas fa-star',
+    name: 'Top Rated',
+    type: 'top_rated'
+  },
+  {
+    id: 4,
+    iconClass: 'fas fa-plus-square',
+    name: 'Upcoming',
+    type: 'upcoming'
+  }
+];
+
 const Header = () => {
+  const [navClass, setNavClass] = useState(false);
+  const [menuClass, setMenuClass] = useState(false);
+
+  const toggleMenu = () => {
+    setNavClass(!navClass);
+    setMenuClass(!menuClass);
+    if (navClass) {
+      document.body.classList.add('header-nav-open');
+    } else {
+      document.body.classList.remove('header-nav-open');
+    }
+  };
+
   return (
     <>
       <div className="header-nav-wrapper">
         <div className="header-bar"></div>
         <div className="header-navbar">
           <div className="header-image">{/* <img src={logo} alt="" /> */}Cinema App</div>
-          <div className="header-menu-toggle" id="header-mobile-menu">
+          <div className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`} id="header-mobile-menu" onClick={() => toggleMenu()}>
             <span className="bar"></span>
             <span className="bar"></span>
             <span className="bar"></span>
           </div>
-          <ul className="header-nav">
+          <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
+            {HEADER_LIST.map((data) => (
+              <li key={data.id}>
+                <span className="header-list-name">
+                  <i className={data.iconClass}></i>
+                </span>
+                &nbsp;
+                <span className="header-list-name">{data.name}</span>
+              </li>
+            ))}
             <li className="header-nav-item">Now Playing</li>
             <li className="header-nav-item">Now Movies</li>
             <input className="search-input" type="text" placeholder="Search for a movie" />

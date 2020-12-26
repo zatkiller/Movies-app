f2ac8c4fd4b395855eb8995fad95d784f505ebe0
zatkiller <gheedylan@yahoo.com.sg>
2020-12-27 00:23:51 +0800

    Added MainContent Component

---
+++ src/components/main-content/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <div className="main-content">
+      <div className="grid-movie-title">
+        <div className="movieType">Now Playing</div>
+        <div className="paginate"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;

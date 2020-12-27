4cf5ce4c98311c75eaac47924e0cadc6e6dc6160
zatkiller <gheedylan@yahoo.com.sg>
2020-12-27 17:53:41 +0800

    Fixed Spinner

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -12,28 +12,28 @@ import { IMAGE_URL } from '../../services/movies.service';
 
 const MainContent = (props) => {
   const { list } = props;
-  // const images = [
-  //   {
-  //     url: 'https://images.pexels.com/photos/4173624/pexels-photo-4173624.jpeg?cs=srgb&dl=pexels-elijah-o%27donnell-4173624.jpg&fm=jpg',
-  //     rating: 7.5
-  //   },
-  //   {
-  //     url: 'https://images.pexels.com/photos/4004374/pexels-photo-4004374.jpeg?cs=srgb&dl=pexels-evie-shaffer-4004374.jpg&fm=jpg',
-  //     rating: 8
-  //   },
-  //   {
-  //     url: 'https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-844297.jpg&fm=jpg',
-  //     rating: 9
-  //   },
-  //   {
-  //     url: 'https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?cs=srgb&dl=pexels-simon-migaj-747964.jpg&fm=jpg',
-  //     rating: 10
-  //   },
-  //   {
-  //     url: 'https://images.pexels.com/photos/1295138/pexels-photo-1295138.jpeg?cs=srgb&dl=pexels-emiliano-arano-1295138.jpg&fm=jpg',
-  //     rating: 6
-  //   }
-  // ];
+  const imagesArray = [
+    {
+      url: 'https://images.pexels.com/photos/4173624/pexels-photo-4173624.jpeg?cs=srgb&dl=pexels-elijah-o%27donnell-4173624.jpg&fm=jpg',
+      rating: 7.5
+    },
+    {
+      url: 'https://images.pexels.com/photos/4004374/pexels-photo-4004374.jpeg?cs=srgb&dl=pexels-evie-shaffer-4004374.jpg&fm=jpg',
+      rating: 8
+    },
+    {
+      url: 'https://images.pexels.com/photos/844297/pexels-photo-844297.jpeg?cs=srgb&dl=pexels-eberhard-grossgasteiger-844297.jpg&fm=jpg',
+      rating: 9
+    },
+    {
+      url: 'https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?cs=srgb&dl=pexels-simon-migaj-747964.jpg&fm=jpg',
+      rating: 10
+    },
+    {
+      url: 'https://images.pexels.com/photos/1295138/pexels-photo-1295138.jpeg?cs=srgb&dl=pexels-emiliano-arano-1295138.jpg&fm=jpg',
+      rating: 6
+    }
+  ];
 
   const [currentPage, setCurrentPage] = useState(1);
   const [images, setImages] = useState([]);
@@ -83,7 +83,7 @@ const MainContent = (props) => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      <Grid images={images} />
+      <Grid images={imagesArray} />
     </div>
   );
 };
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MainContent from '../main-content/MainContent';
+import Spinner from '../spinner/Spinner';
 
 const Main = () => {
-  return (
-    <div className="main">
-      <MainContent />
-    </div>
-  );
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
+  return <div className="main">{loading ? <Spinner /> : <MainContent />}</div>;
 };
 
 export default Main;

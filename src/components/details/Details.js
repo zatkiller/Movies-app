204115d98148c85537d964a820a51aca948e878c
zatkiller <gheedylan@yahoo.com.sg>
2020-12-28 23:30:58 +0800

    Added Detail Components and sttyling

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,10 +1,45 @@
 import React from 'react';
 
+import './Details.scss';
+import Rating from '../rating/Rating';
+
 const Details = () => {
   return (
-    <div>
-      <h1>Details</h1>
-    </div>
+    <>
+      <div className="movie-container">
+        <div
+          className="movie-bg"
+          style={{
+            backgroundImage: 'url(https://images.unsplash.com/photo-1545776312-71c1641b3849?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1064&q=80)'
+          }}
+        ></div>
+        <div className="movie-overlay"></div>
+        <div className="movie-details">
+          <div className="movie-image">
+            <img src="https://images.unsplash.com/photo-1545776312-71c1641b3849?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1064&q=80" alt="" />
+          </div>
+          <div className="movie-body">
+            <div className="movie-overview">
+              <div className="title">
+                Avengers <span>2020-12-03</span>
+              </div>
+              <div className="movie-genres">
+                <ul className="genres">
+                  <li>Action</li>
+                  <li>Comedy</li>
+                  <li>Sci-fi</li>
+                </ul>
+              </div>
+              <div className="rating">
+                <Rating className="rating-stars" rating={6.5} totalStars={10} />
+                &nbsp;
+                <span>6.5</span> <p>(200) reviews</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 

e5e6aa62b326c4c4495d05c7d976d750b3dc8ec8
zatkiller <gheedylan@yahoo.com.sg>
2020-12-29 22:48:18 +0800

    Added syling for eror component

--- src/components/error/ErrorPage.js
+++ src/components/error/ErrorPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './ErrorPage.scss';
+
 const ErrorPage = () => {
   return (
     <div className="error-page">
       <h1 className="error-header">Oops!</h1>
       <p className="error-msg">Something went wrong.</p>
-      <Link to={'/'}>
+      <Link to={'/'} className="error-link">
         <i className="icon-home"></i> Go back to home page.
       </Link>
     </div>
